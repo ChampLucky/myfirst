74a5ae8873cadc992f663a64acf3434bad2430b8
lucky <bourasilucky@gmail.com>
2021-02-18 19:34:08 +0530

    changes made in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -9,7 +9,7 @@ cout<<"welcome to Newway"<<endl;
 
 cout<<"sid and ojaswa"<<endl;
 cout<<"let me write something and create a pull request";
-
+COUT<<"LET THEM SHINE";
   //done with the changes
 
 return 0;
